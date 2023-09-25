73176b7dc36d5a588ec4444125cdca0312895dde
JoaoVictorVila-FP <jvilas22@fpcoruna.afundacion.org>
2023-09-25 12:00:45 +0200

    Ex 1 de python

--- adivinanza.py
+++ adivinanza.py
@@ -1,1 +1,18 @@
 
+
+
+print("Una adivinanza: Tengo hojas pero no soy un árbol, tengo una cara pero no soy una persona, ¿qué soy?" )
+print("a) Un libro\nb) Una montaña\nc) Una nube\n\n\n")
+
+r = input("Respuesta: ")
+
+
+if r == "a":
+    print("Correcto!")
+else:
+    print("Incorrecto")
+
+
+
+
+
