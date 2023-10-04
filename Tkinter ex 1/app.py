4629396246e4bf73a2b7f0cc088d85d1c8c4e2f9
JoaoVictorVila-FP <jvilas22@fpcoruna.afundacion.org>
2023-10-04 09:30:06 +0200

    Hello world tkinter

---
+++ Tkinter ex 1/app.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        
+        self.title("Hello World")
+
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
+
+
+
+
+
