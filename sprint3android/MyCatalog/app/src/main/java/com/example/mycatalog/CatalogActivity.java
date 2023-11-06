abdeabd42910134e0b36049d33496778316b85d0
JoaoVictorVila-FP <jvilas22@fpcoruna.afundacion.org>
2023-11-06 17:14:48 +0100

    Tarea 2 Sprint 3

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,14 +1,45 @@
 package com.example.mycatalog;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 
 public class CatalogActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
+
+
+
+
+        View btn1 = findViewById(R.id.btn1);
+
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+
+
+
+
+
+
+            }
+
+
+        });
     }
 }
