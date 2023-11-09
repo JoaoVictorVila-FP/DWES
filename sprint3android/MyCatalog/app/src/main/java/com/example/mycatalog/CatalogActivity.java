4178003df622ee04210f496cf60ee7c0f04380ab
JoaoVictorVila-FP <jvilas22@fpcoruna.afundacion.org>
2023-11-09 14:46:21 +0100

    Tarea 5 sprint 3

--- sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/MyCatalog/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,16 +1,24 @@
 package com.example.mycatalog;
 
 
-import androidx.appcompat.app.AppCompatActivity;
-
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-public class CatalogActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class CatalogActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener  {
+
 
 
     @Override
@@ -18,16 +26,12 @@ public class CatalogActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_catalog);
 
-
-
-
         View btn1 = findViewById(R.id.btn1);
-
 
         btn1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(CatalogActivity.this, "Button clicked", Toast.LENGTH_SHORT).show();
 
                 Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
                 startActivity(intent);
@@ -36,5 +40,62 @@ public class CatalogActivity extends AppCompatActivity {
 
 
         });
+
+
+
+
+
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        if (savedInstanceState == null) {
+
+            Fragment fragment = new AboutFragment();
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.nav_host_fragment, fragment)
+                    .commit();
+
+            navigationView.setCheckedItem(R.id.nav_fragment1);
+        }
+    }
+
+    @Override
+
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        int id = item.getItemId();
+
+        if (id == R.id.nav_fragment1) {
+            fragment = new AboutFragment();
+        } else if (id == R.id.nav_fragment2) {
+
+            fragment = new Fragment();
+        }
+
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.nav_host_fragment, fragment)
+                    .commit();
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
