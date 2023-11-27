a36f3908f9585b8e18608e4ec2b0d1b72c9c32ac
JoaoVictorVila-FP <jvilas22@fpcoruna.afundacion.org>
2023-11-27 11:41:28 +0100

    CONNECTION TEST

---
+++ sprint4android/app/src/main/java/com/example/myothercatalog/MainActivity.java
@@ -0,0 +1,90 @@
+package com.example.myothercatalog;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+
+
+
+
+// Main activity for the application
+public class MainActivity extends AppCompatActivity {
+
+    // RecyclerView instance
+    private RecyclerView recyclerView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Set the content view for the activity
+        setContentView(R.layout.activity_main);
+
+        // Initialize RecyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        // Make API request to fetch data and populate RecyclerView
+        makeRequest();
+    }
+
+    // Function to make a request to an API endpoint and populate the RecyclerView
+    private void makeRequest() {
+        // List to store data from the API response
+        List<MyItem> itemList = new ArrayList<>();
+
+        // Create a request to fetch JSON data from the specified URL
+        JsonArrayRequest request = new JsonArrayRequest
+                (Request.Method.GET,
+                        "https://raw.githubusercontent.com/JoaoVictorVila-FP/DWES/main/recursos/catalog.json",
+                        null,
+                        new Response.Listener<JSONArray>() {
+                            @Override
+                            public void onResponse(JSONArray response) {
+                                try {
+                                    // Parse the JSON response and create MyItem objects
+                                    for (int i = 0; i < response.length(); i++) {
+                                        JSONObject jsonObject = response.getJSONObject(i);
+                                        MyItem myItem = new MyItem(jsonObject);
+                                        itemList.add(myItem);
+                                    }
+
+                                    // Set up the RecyclerView with the populated data
+                                    MyAdapter myAdapter = new MyAdapter(itemList, MainActivity.this);
+                                    recyclerView.setAdapter(myAdapter);
+                                    recyclerView.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        }
+                );
+
+        // Add the request to the Volley request queue
+        Volley.newRequestQueue(this).add(request);
+    }
+}
---
+++ sprint4android/app/src/main/java/com/example/myothercatalog/MyAdapter.java
@@ -0,0 +1,68 @@
+// Import statements
+package com.example.myothercatalog;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+// Adapter class for the RecyclerView
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    // List of items and other necessary variables
+    private final List<MyItem> cod_list;
+    private final LayoutInflater layoutInflater;
+    private final Context context;
+
+    // Constructor to initialize the adapter
+    public MyAdapter(List<MyItem> cod_list, Context context) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.cod_list = cod_list;
+        this.context = context;
+    }
+
+    // Inflates the layout for each item in the RecyclerView
+    @NonNull
+    @Override
+    public MyAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.item_layout, null);
+        return new MyViewHolder(view);
+    }
+
+    // Binds data to the views in each item of the RecyclerView
+    @Override
+    public void onBindViewHolder(@NonNull MyAdapter.MyViewHolder holder, int position) {
+        MyItem MyItem = cod_list.get(position);
+        holder.title.setText(MyItem.getTitle());
+        Glide.with(context).load(MyItem.getUrl()).into(holder.image);
+    }
+
+    // Returns the total number of items in the RecyclerView
+    @Override
+    public int getItemCount() {
+        return cod_list.size();
+    }
+
+    // ViewHolder class to hold references to the views in each item
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView image;
+        TextView title;
+
+        // Constructor to initialize the ViewHolder
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            image = itemView.findViewById(R.id.iconImageView);
+            title = itemView.findViewById(R.id.cod_title);
+        }
+    }
+}
